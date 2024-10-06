c1a7538c54a5fe9e0bf6833769d003755c660347
Zeeshan Akram <zeeshan331578@gmail.com>
2024-10-07 00:36:30 +0500

    first-commit

--- app.js
+++ app.js
@@ -5,17 +5,24 @@ import express from "express";
 import bodyParser from "body-parser";
 import ejs from "ejs";
 import mongoose from "mongoose";
+import md5 from "md5"
+import bcrypt from "bcrypt"
 
-
+const saltRounds = 5
 const postSchema = new mongoose.Schema({
   title: String,
   content: String
 });
 
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String
+});
+ 
 let conn = await mongoose.connect(process.env.MONGODB)
 
 const Posts = mongoose.model('Post', postSchema);
-
+const User = mongoose.model('User', userSchema);
 
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
@@ -27,7 +34,9 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-let posts = [];
+
+
+
 
 app.get("/", async (req,res) => {
   let allPosts = await Posts.find({});
@@ -38,6 +47,65 @@ app.get("/", async (req,res) => {
     res.render("home2", {posts1: allPosts})
   }
 })
+
+app.get("/register", function(req,res) {
+  res.render("register")
+})
+
+
+app.post("/register", async (req,res) => {
+
+  bcrypt.hash(req.body.password, saltRounds, async (err,hash)=>{
+    let newUser =  {
+      userEmail: req.body.email,
+      userPassword: hash
+    }
+    
+  await User.create({ email: newUser['userEmail'], password: newUser['userPassword']})
+  if(err){
+    console.log(err)
+  }else{
+    res.redirect("/")
+  }
+
+  })
+})
+
+app.get("/login", function(req,res) {
+  res.render("login")
+})
+
+
+app.post("/login", async (req,res) => {
+    let userEmail= req.body.email
+    let userPassword= req.body.password
+
+    let foundUser = await User.findOne({email: userEmail} )
+   
+    bcrypt.compare(userPassword,foundUser.password,function(err,result){
+      if(result===true){
+        console.log("successfully logged in")
+        res.redirect("/")
+      }else{
+        res.render("wrongcredentials")
+      }
+    })
+  })
+
+
+
+        // if(foundUser){
+        //   if(foundUser.password === userPassword){
+        //     res.render("home")
+        //   }else{
+        //     res.render("wrongcredentials")
+        //   }
+        // }else{
+        //   res.render("wrongcredentials")
+        // }
+  
+   
+  // res.redirect("/")
 
 
 app.get("/about", function(req,res) {
